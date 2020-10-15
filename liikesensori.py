6a3d795e55b6a1f5474249228963884193610fa8
Maiju Valkeinen <maiju.valkeinen@gmail.com>
2020-10-15 19:21:33 +0300

    muokkasin tiedostoon kirjoittamista vähän turvallisemmaksi

--- liikesensori.py
+++ liikesensori.py
@@ -8,10 +8,7 @@ pin = 5                             #mones pinni piirilevyllä OUT (keltainen jo
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.IN)
 
-try:
-    tiedosto = open("puistoseuranta.txt", "x") #tekee uuden tiedoston
-except:
-    tiedosto = open("puistoseuranta.txt", "a") #jos tiedosto on jo olemassa
+
 
 try:
     while True:
@@ -21,12 +18,20 @@ try:
           #  print ("Ei liiku"),i
             time.sleep(1)
         else:                       #selsori on havainnut liikkeen
+            try:
+                tiedosto = open("puistoseuranta.txt", "x") #tekee uuden tiedoston
+            except:
+                tiedosto = open("puistoseuranta.txt", "a") #jos tiedosto on jo olemassa
+            #tämä on hitaampaa, mutta turvallisempaa kirjoittaa tiedostoon suorituksen aikana
+                
+                
             tiedosto.write(str(lID) + "|" + aika.strftime("%y-%m-%d %H:%M:%S") + "; \n"),i
+            tiedosto.close()
             time.sleep(3)           #liikesensorin pienin aika on 2,5 s 
 except:
     pass
 
-tiedosto.close()
+
 GPIO.cleanup()
 
 #TODO: tuloste tulee tuplana ~Maiju 15.10.
