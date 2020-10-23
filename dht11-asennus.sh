3732128b3478a6bcd940cde9be9e1f8787bbc59f
tonimoil <62765827+tonimoil@users.noreply.github.com>
2020-10-23 17:08:45 +0300

    Add files via upload

---
+++ dht11-asennus.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install sqlite3
+sudo pip3 install dataset
+sudo pip3 install adafruit-circuitpython-dht
+sudo apt-get install libgpiod2
+sudo pip3 install timeloop
