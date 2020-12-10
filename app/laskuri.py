63d4b4e3eef49834576603db273c433a0385534b
Maisa <maiju.valkeinen@gmail.com>
2020-12-10 22:21:14 +0200

    Vielä yksi yritys korjata laskuri

--- app/laskuri.py
+++ app/laskuri.py
@@ -3,38 +3,38 @@ from sqlite3 import Error
 from datetime import datetime, timedelta
 
 
-#Ottaa tietokannan sijainnin ja muodostetaan yhteyden siihen
+
 def yhdista(data):
     yhteys = None
     try:
         yhteys = sqlite3.connect(data)
     except Error as e:
         print(e)
-        return yhteys
-    
+    return yhteys
+                                           
 
-#Ottaa vastaan yhteyden ja palauttaa viiden minuutin sisällä tehdyt merkinnät
 def select_rivit(yhteys):
-    nyt = datetime.now()
-    viisi = timedelta(minutes=5) 
-    nyt = nyt-viisi
-    stime = nyt.strftime("%Y-%m-%d %H:%M:%S")
-    cur = yhteys.curso()
-    komento="SELECT COUNT(aika) FROM sensor_data WHERE sisaan == 1 AND Datetime(aika)>=Datetime('now','+2 hours', '-5 minutes')" #lisätään +2, koska muuten näyttää väärää aikaa
+    cur = yhteys.cursor()
+    komento="SELECT COUNT(aika) FROM sensor_data WHERE sisaan == 1 AND Datetime(aika)>=Datetime('now','+2 hours', '-5 minutes')"
     # print(komento)
     cur.execute(komento)
     tulos = cur.fetchone()
-    return tulos[0] 
+    return tulos                                                 
 
 
 def main():           
-    tk = "/home/humppa/puistoseuranta/instance/app.sqlite"
+    tk = "../instance/app.sqlite"
     yhteys = yhdista(tk)
     with yhteys:
         hetkellinen=select_rivit(yhteys)
-    return hetkellinen
- #    return int(42)
-        
+#        print(hetkellinen[0]) 
+    return hetkellinen[0]
+   
+
 if __name__ == '__main__':
     main()
+
+
+
+
 
